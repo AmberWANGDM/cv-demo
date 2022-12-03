cc032986df8735376f08baf9416ca0a30da51f74
WangDanmeng <WangDanmeng98@163.com>
2022-12-03 21:21:18 +0800

    打印一段话

--- readme.md
+++ readme.md
@@ -1,2 +1,4 @@
+```bash
 yarn global add parcel@1.9.7
 parcel build src/index.html --no-minify --public-url .
+```
--- src/main.js
+++ src/main.js
@@ -1,17 +1,29 @@
 let demo = document.querySelector('#demo')
-let n = 0
-let string = '你好, 我是一名前端新人'
-demo.innerHTML = string.substring(0, n) // 左闭右开
+let n = -1
+let string = `你好, 我是一名前端新人
+接下来我要加样式了
+我要加的样式是
+body {
+  color:red;
+}
+`
+let showString = ''
+// string = string.replace(/\n/g, '<br>')
+demo.innerHTML = string.substring(0, n)
 
 let step = () => {
   setTimeout(() => {
-    if (n >= string.length) return
+    if (n >= string.length - 1) return
     n = n + 1
-    demo.innerHTML = string.substring(0, n)
-    // 递归
+    if (string[n] === '\n') {
+      // 如果是换行,就加换行标签
+      showString += '<br>'
+    } else {
+      showString += string[n]
+    }
+    demo.innerHTML = showString
     step()
-  }, 200)
+  }, 50)
 }
 
 step()
-// step() // 注意这样写会在1s后变成3
